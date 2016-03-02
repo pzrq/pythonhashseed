b0c456a0bae76a285f81196a46d0f4e1581228b5
Peter Schmidt <peter@peterjs.com>
2016-03-02 22:12:08 +1100

    Add discover_road_runner
    Let's get some traffic light colours for the demo, because so much easier to read.

--- demo/settings.py
+++ demo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,6 +82,18 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        },
+    }
+    TEST_RUNNER = 'discover_road_runner.runners.DiscoverRoadRunner'
 
 
 # Internationalization
