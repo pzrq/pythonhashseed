358781e24ce51a96152fd47dde692ab6a7e9b494
Peter Schmidt <peter@peterjs.com>
2016-03-02 22:38:08 +1100

    One possible fix

--- python_hash_seed/tests.py
+++ python_hash_seed/tests.py
@@ -4,5 +4,5 @@ from django.test import TestCase
 class PythonHashSeedTest(TestCase):
     def test_sometimes_passes(self):
         expect = ['a', 'bb']
-        actual = list({'a': 5, 'bb': 8}.keys())
+        actual = sorted({'a': 5, 'bb': 8}.keys())
         self.assertEqual(expect, actual)
