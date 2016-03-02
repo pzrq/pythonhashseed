302ae960309447a31d2abb048e6b4863519a8186
Peter Schmidt <peter@peterjs.com>
2016-03-02 22:00:15 +1100

    Add PythonHashSeedTest.test_sometimes_passes

--- python_hash_seed/tests.py
+++ python_hash_seed/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class PythonHashSeedTest(TestCase):
+    def test_sometimes_passes(self):
+        expect = ['a', 'bb']
+        actual = list({'a': 5, 'bb': 8}.keys())
+        self.assertEqual(expect, actual)
